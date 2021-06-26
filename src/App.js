32e075987b86d08274018b1b1083c35df7555954
Cathy <catherinecasey513@yahoo.com>
2021-06-26 13:13:32 -0500

    Initial inclusion of React Select

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Select from 'react-select';
 
 function App() {
+  const options = [
+    {
+      value: 'value1',
+      label: 'Label 1',
+    }
+  ]
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{width: '15rem'}}>
+          <Select 
+            options={options}
+            placeholder='My Select'
+          />
+        </div>
       </header>
     </div>
   );
